0eeaf0cbf8c7b1fa5e7b2bc91a9156d95ca9a532
raphywsk/works <raphywsk@icloud.com>
2022-11-04 18:02:23 -0300

    feat: add fakeStore api in environments

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  FAKE_STORE_API: 'https://fakestoreapi.com/'
 };
 
 /*
