5e33d8c67e6f870de9c887901de91734c68fecd6
raphywsk/works <raphywsk@icloud.com>
2022-11-19 15:43:42 -0300

    chore(header component): moves header component to store component
    Removes header component from app component and insert header component in store component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,21 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
-import { MatToolbarModule } from '@angular/material/toolbar';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './components/header/header.component';
-import { MatIconModule } from '@angular/material/icon';
+
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HeaderComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatToolbarModule,
     HttpClientModule,
-    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/store/store.module.ts
+++ src/app/store/store.module.ts
@@ -1,24 +1,27 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatSelectModule } from '@angular/material/select';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
+import { HeaderComponent } from '../components/header/header.component';
 import { ProductDetailsComponent } from './product-details/product-details.component';
 import { ProductsComponent } from './products/products.component';
 import { StoreRoutingModule } from './store-routing.module';
 import { StoreComponent } from './store/store.component';
-import { MatButtonModule } from '@angular/material/button';
 
 
 @NgModule({
   declarations: [
     StoreComponent,
     ProductsComponent,
-    ProductDetailsComponent
+    ProductDetailsComponent,
+    HeaderComponent
   ],
   imports: [
     CommonModule,
@@ -29,7 +32,8 @@ import { MatButtonModule } from '@angular/material/button';
     MatSelectModule,
     FormsModule,
     MatIconModule,
-    MatButtonModule
+    MatButtonModule,
+    MatToolbarModule
   ]
 })
 export class StoreModule { }
